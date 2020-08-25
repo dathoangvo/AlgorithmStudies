db8fa8c1560c82809ecdd7a8025fade06c1cbf76
dathoangvo <56531898+dathoangvo@users.noreply.github.com>
2020-08-25 12:23:07 -0700

    Add files via upload

---
+++ src/sample/Main.java
@@ -0,0 +1,103 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private int screenWidth = 800;
+    private int screenHeight = 800;
+
+    public int mode = 0;
+
+    private drawWQUPC WQUPCl = new drawWQUPC(10, 10, screenWidth, screenHeight);;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Algorithm Visualizer");
+        //primaryStage.setScene(new Scene(root, 500, 500));
+
+        //drawWQUPC grid = new drawWQUPC(10, 10, 500, 500);
+
+
+        //grid.addStage(primaryStage);
+
+        //primaryStage.setScene(grid.getScene());
+
+        Rectangle QuickUnion = new Rectangle();
+        QuickUnion.setX(350);
+        QuickUnion.setY(50);
+        QuickUnion.setWidth(100);
+        QuickUnion.setHeight(50);
+        QuickUnion.setFill(Color.color(0, 1, 1));
+
+        Text quickUnionText = new Text("Quick Union");
+        quickUnionText.setX(360);
+        quickUnionText.setY(80);
+        updateContent(primaryStage);
+
+        Group group = new Group(QuickUnion, quickUnionText);
+        Scene j = new Scene(group, screenWidth, screenHeight);
+        primaryStage.setScene(j);
+        QuickUnion.addEventFilter(MouseEvent.MOUSE_CLICKED, chooseAlgorithm(primaryStage,0));
+        quickUnionText.addEventFilter(MouseEvent.MOUSE_CLICKED, chooseAlgorithm(primaryStage, 0));
+        primaryStage.addEventFilter(MouseEvent.MOUSE_MOVED, chooseAlgorithm(primaryStage, 99));
+        //primaryStage.addEventFilter(MouseEvent.MOUSE_MOVED, updateGraphics(primaryStage));
+
+    }
+
+    public javafx.event.EventHandler<MouseEvent> updateGraphics(Stage primaryStage) {
+        if (mode == 0) {
+            return null;
+        } else if (mode == 1) {
+            primaryStage.setScene(WQUPCl.getScene());
+        }
+
+        return null;
+    }
+
+    public javafx.event.EventHandler<MouseEvent> chooseAlgorithm(Stage primaryStage, int pick) {
+        //EventHandler<MouseEvent> chosenAlgorithm = null;
+        //drawWQUPC WQUPCl = new drawWQUPC(10, 10, 500, 500);
+
+        if (pick == 0) {
+            return mouseEvent -> {
+                mode = 1;
+                primaryStage.setScene(WQUPCl.getScene());
+                //updateContent(primaryStage);
+            };
+
+        } else if (pick == 1) {
+            return mouseEvent ->
+                    System.out.println("EmptyAlgo1");
+        } else if (pick == 99) {
+            return mouseEvent -> {
+                updateGraphics(primaryStage);
+            };
+        }
+
+        return null;
+    }
+
+
+    public void updateContent(Stage primaryStage) {
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
---
+++ src/sample/drawWQUPC.java
@@ -0,0 +1,108 @@
+package sample;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+
+public class drawWQUPC {
+    private int gridWidth;
+    private int gridHeight;
+
+    private int screenWidth;
+    private int screenHeight;
+
+    private WeightQUPathCompress grid;
+
+    private Scene scene;
+
+    private Group group;
+
+
+    public drawWQUPC(int init_gridWidth, int init_gridHeight, int init_screenWidth, int init_screenHeight) {
+        gridWidth = init_gridWidth;
+        gridHeight = init_gridHeight;
+        screenWidth = init_screenWidth;
+        screenHeight = init_screenHeight;
+
+        grid = new WeightQUPathCompress(gridWidth, gridHeight);
+
+        group = new Group(updateGrid());
+        group.getChildren().addAll(updatePosition());
+
+        scene = new Scene(group, screenWidth, screenHeight);
+        scene.setFill(Color.color(.6, 0, 0));
+    }
+
+    public javafx.event.EventHandler<MouseEvent> event(int pos) {
+        return mouseEvent -> {
+            grid.updateOpen(pos);
+            group = new Group(updateGrid());
+            group.getChildren().addAll(updatePosition());
+            scene = new Scene(group, screenWidth, screenHeight);
+            scene.setFill(Color.color(0, 0, 0));
+
+        };
+    }
+
+    public Text[] updatePosition() {
+        Text[] gridPosition = new Text[grid.size()];
+        int horizontal = 0;
+        int vertical = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            gridPosition[i] = new Text(Integer.toString(grid.getParent(i)));
+            gridPosition[i].setFill(Color.color(0, 0, 0));
+            if (horizontal == gridWidth) {
+                vertical++;
+                horizontal = 0;
+            }
+
+            gridPosition[i].setX(horizontal * (int) (screenWidth / gridWidth) + 20);
+            gridPosition[i].setY(vertical * (int) (screenHeight / gridHeight) + 20);
+
+            horizontal++;
+        }
+
+        return gridPosition;
+    }
+
+    public javafx.scene.shape.Rectangle[] updateGrid() {
+        javafx.scene.shape.Rectangle[] returnGrid = new javafx.scene.shape.Rectangle[grid.size()];
+        int horizontal = 0;
+        int vertical = 0;
+
+        //grid.updateOpen(0);
+
+        for (int i = 0; i < grid.size(); i++) {
+
+            returnGrid[i] = new javafx.scene.shape.Rectangle();
+            if (horizontal == gridWidth) {
+                vertical++;
+                horizontal = 0;
+            }
+            if (grid.checkOpen(i)) {
+                returnGrid[i].setFill(Color.color(0, .6, .9));
+            } else {
+                returnGrid[i].setFill(Color.color(1, 1, 1));
+            }
+            returnGrid[i].setX(horizontal * (int) (screenWidth / gridWidth) + 1);
+            returnGrid[i].setY(vertical * (int) (screenHeight / gridHeight) + 1);
+            returnGrid[i].setWidth((int) (screenWidth / gridWidth) - 2);
+            returnGrid[i].setHeight((int) (screenHeight / gridHeight) - 2);
+
+
+
+            returnGrid[i].addEventFilter(MouseEvent.MOUSE_CLICKED, event(i));
+
+            horizontal++;
+        }
+        return returnGrid;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
